0f4ca6b782a8d26dc29a46d3049f99944f64c4cc
zhangmengzhi <104446930@qq.com>
2016-01-22 20:55:40 +0800

    添加*.orm.mapper.AccountMapper注释

--- src/main/java/org/zhangmz/pickles/orm/mapper/AccountMapper.java
+++ src/main/java/org/zhangmz/pickles/orm/mapper/AccountMapper.java
@@ -3,5 +3,15 @@ package org.zhangmz.pickles.orm.mapper;
 import org.zhangmz.pickles.orm.model.Account;
 import org.zhangmz.pickles.orm.MyMapper;
 
+/**
+ * 
+ * @ClassName:AccountMapper 
+ * @Description:帐户（account）的操作类
+ * @author:张孟志
+ * @date:2016年1月22日 下午8:48:43 
+ * @version V1.0
+ * 说明：帐户（account）的操作类
+ *     开发代码用自https://github.com/abel533/MyBatis-Spring-Boot
+ */
 public interface AccountMapper extends MyMapper<Account> {
 }
