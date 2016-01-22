c0a59e974084a540269d0a3fca4bd6006578181b
zhangmengzhi <104446930@qq.com>
2016-01-22 20:21:01 +0800

    搭建数据库环境。

--- src/main/java/org/zhangmz/pickles/PicklesApplication.java
+++ src/main/java/org/zhangmz/pickles/PicklesApplication.java
@@ -1,13 +1,62 @@
 package org.zhangmz.pickles;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+
 /**
- * Hello world!
- *
+ * 
+ * @ClassName:PicklesApplication 
+ * @Description:SpringBoot 应用
+ * @author:张孟志
+ * @date:2016年1月22日 下午7:42:52 
+ * @version V1.0
+ * 说明：启动SpringBoot 应用入口
  */
-public class PicklesApplication 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class PicklesApplication {
+	
+	/**
+	 * 启动嵌入式的Tomcat并初始化Spring环境.
+	 * 
+	 * 无 applicationContext.xml 和 web.xml, 靠下述方式进行配置：
+	 * 
+	 * 1. 扫描当前package下的class设置自动注入的Bean<br/>
+	 * 2. 也支持用@Bean标注的类配置Bean <br/>
+	 * 3. 根据classpath中的三方包Class及集中的application.properties条件配置三方包，如线程池 <br/>
+	 * 4. 也支持用@Configuration标注的类配置三方包.
+	 */
+	public static void main(String[] args) {
+        SpringApplication.run(PicklesApplication.class, args);
     }
+
+	/**
+	 * 
+	 * @Title: containerCustomizer 
+	 * @Description: 自定义常见错误页面
+	 * @return
+	 * @throws 
+	 * 增加人:张孟志
+	 * 增加日期:2016年1月13日 上午9:57:19
+	 * 说明：自定义常见错误页面 
+	 *     error401Page, error403Page, error404Page, error500Page
+	 *     注意这个方法使用Java 8的lambda表达式来简化实现的方式。
+	 */
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+	   return (container -> {
+    		ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, "/static/400.html");
+	        ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/static/401.html");
+	        ErrorPage error403Page = new ErrorPage(HttpStatus.FORBIDDEN, "/static/403.html");
+	        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/static/404.html");
+	        ErrorPage error405Page = new ErrorPage(HttpStatus.METHOD_NOT_ALLOWED, "/static/405.html");
+	        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/static/500.html");
+
+	        container.addErrorPages(error400Page, error401Page, error403Page, error404Page, error405Page, error500Page);
+	   });
+	}
 }
---
+++ src/main/resources/mysql_demo.sql
@@ -0,0 +1,48 @@
+-- CREATE DATABASE `pickles` /*!40100 COLLATE 'utf8_unicode_ci' */
+-- CREATE USER 'pickles'@'localhost' IDENTIFIED BY 'pickles';
+-- GRANT USAGE ON *.* TO 'pickles'@'localhost';
+-- GRANT SELECT, EXECUTE, SHOW VIEW, ALTER, ALTER ROUTINE, CREATE, CREATE ROUTINE, CREATE TEMPORARY TABLES, CREATE VIEW, DELETE, DROP, EVENT, INDEX, INSERT, REFERENCES, TRIGGER, UPDATE, LOCK TABLES  ON `pickles`.* TO 'pickles'@'localhost' WITH GRANT OPTION;
+-- FLUSH PRIVILEGES;
+
+drop table if exists account;
+
+create table account (
+	id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	phone varchar(20) NOT NULL,
+	name varchar(64) DEFAULT '新用户',
+	email varchar(64),
+	hash_password varchar(128) NOT NULL,
+	salt varchar(16) NOT NULL,
+	register_date timestamp DEFAULT CURRENT_TIMESTAMP() COMMENT '注册日期',
+	group_id INT(3) null DEFAULT 999 COMMENT '组ID，确认权限',
+	status varchar(4) DEFAULT 'N',
+	UNIQUE index_account_phone (phone),
+	UNIQUE index_account_email (email)
+);
+
+insert into account (id,phone,email,name,hash_password,salt,group_id,status) values(1,'13000000001','1@gmail.com','管理员','48577990717ce63726608c92c6ab257f','4sG3vd',1,'Y');
+insert into account (id,phone,email,name,hash_password,salt,status) values(2,'13000000002','2@gmail.com','东方不败','48577990717ce63726608c92c6ab257f','4sG3vd','Y');
+insert into account (id,phone,email,name,hash_password,salt,status) values(3,'13000000003','3@gmail.com','张三','48577990717ce63726608c92c6ab257f','4sG3vd','Y');
+insert into account (id,phone,email,name,hash_password,salt) values(4,'13000000004','4@gmail.com','李四','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(5,'13000000005','5@gmail.com','王五','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(6,'13000000006','6@gmail.com','赵六','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt,group_id) values(7,'13000000007','7@gmail.com','管理员2','48577990717ce63726608c92c6ab257f','4sG3vd',2);
+insert into account (id,phone,email,name,hash_password,salt) values(8,'13000000008','8@gmail.com','东方不败','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(9,'13000000009','9@gmail.com','张三','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(10,'13000000010','10@gmail.com','李四','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(11,'13000000011','11@gmail.com','王五','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(12,'13000000012','12@gmail.com','赵六','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt,group_id) values(13,'13000000013','13@gmail.com','管理员3','48577990717ce63726608c92c6ab257f','4sG3vd',2);
+insert into account (id,phone,email,name,hash_password,salt) values(14,'13000000014','14@gmail.com','东方不败','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(15,'13000000015','15@gmail.com','张三','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(16,'13000000016','16@gmail.com','李四','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(17,'13000000017','17@gmail.com','王五','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(18,'13000000018','18@gmail.com','赵六','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt,group_id) values(19,'13000000019','19@gmail.com','管理员4','48577990717ce63726608c92c6ab257f','4sG3vd',2);
+insert into account (id,phone,email,name,hash_password,salt) values(20,'13000000020','20@gmail.com','东方不败','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(21,'13000000021','21@gmail.com','张三','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(22,'13000000022','22@gmail.com','李四','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(23,'13000000023','23@gmail.com','王五','48577990717ce63726608c92c6ab257f','4sG3vd');
+insert into account (id,phone,email,name,hash_password,salt) values(24,'13000000024','24@gmail.com','赵六','48577990717ce63726608c92c6ab257f','4sG3vd');
+
+commit;
