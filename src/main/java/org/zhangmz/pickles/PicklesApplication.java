dfbb4ffd15bdfe1e685a4f3463f02af39b9d17cf
zhangmengzhi <104446930@qq.com>
2016-01-22 19:24:16 +0800

    今晚的新咸菜有点青辣，味道不错。项目初建。

---
+++ src/main/java/org/zhangmz/pickles/PicklesApplication.java
@@ -0,0 +1,13 @@
+package org.zhangmz.pickles;
+
+/**
+ * Hello world!
+ *
+ */
+public class PicklesApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
