6e4a21d4032c69fff4fc5298e89d8ae7d7506c95
张孟志 <104446930@qq.com>
2016-03-11 11:59:32 +0800

    制定终端通信协议

--- src/main/java/org/zhangmz/pickles/api/channel/ChannelServiceRestController.java
+++ src/main/java/org/zhangmz/pickles/api/channel/ChannelServiceRestController.java
@@ -17,6 +17,48 @@ import org.zhangmz.pickles.modules.convert.JsonMapper;
  * @version V1.0
  * 说明：渠道服务接入口
  * 手机/平板（手持设备）、PC客户端/机器人（APP）、等多种渠道数据入口
+ * 
+ 接口请求地址: http://xxx.xxx.xxx/api/channel/service
+接口请求方式: HTTP POST
+接口报文编码: UTF-8
+请求报文格式:
+	请求报文有且仅有如下的字段，各字段分别解释如下：
+	_channel_		表示请求来源区分，手机/平板（手持设备）、PC客户端/机器人（APP）等
+	_version_ 		接口版本
+	_token_			认证码，登陆成功后系统会返回一个认证码
+	_code_			接口服务代码，唯一标示一个接口
+	_data_			base64编码后的业务数据
+为方便说明，我们将_data_外的所有字段称为"基础字段"。 
+base64编码前的_data_格式是如下的json(所有的json的键都要加双引号，值不一定)
+{
+  "value" : {
+    "datatime" : "2014-09-23 09:11:11",
+    "address" : "xxx"
+  }
+}
+
+返回报文格式：
+返回报文是json格式，包含2个字段，code、message、resultMap，
+code表示状态（1表示成功，0表示失败），message表示提示信息，resultMap是返回的业务数据（或错误提示信息）。
+成功报文示例：
+{
+	"code":"1",
+	"message":"成功",
+	"resultMap": {
+		"customservice": "(010)62693990",
+		"dataversion": 131
+	}
+}
+
+失败报文示例：
+
+{
+	"code":"0",
+	"message":"失败",
+	"resultMap": {
+		"result":"e3b6d2afc7d34af5a5ec3ff3253e1ee2"
+	}
+}
  */
 @RestController
 @RequestMapping("/api/channel/service")
